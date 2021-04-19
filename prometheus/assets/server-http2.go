6c989010b127d316f1457794d19a34f5f14cd68b
Pavesi, Valdemar Moreira <valdemar.pavesi@one.verizon.com>
2021-04-19 17:47:19 -0500

    u

--- prometheus/assets/server-http2.go
+++ prometheus/assets/server-http2.go
@@ -1,75 +1,15 @@
 package main
 
 import (
-	"context"
 	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
-
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/stdout"
-	"go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/metric/global"
-	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
-	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
-	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
 )
 
 func main() {
-
-	exporter, err := stdout.NewExporter(
-		stdout.WithPrettyPrint(),
-	)
-	if err != nil {
-
-		log.Fatalf("failed to initialize stdout export pipeline: %v", err)
-	}
-
-	pusher := controller.New(
-		processor.New(
-			simple.NewWithExactDistribution(),
-			exporter,
-		),
-		controller.WithExporter(exporter),
-		controller.WithCollectPeriod(5*time.Second),
-	)
-
-	err = pusher.Start(ctx)
-	if err != nil {
-		log.Fatalf("failed to initialize metric controller: %v", err)
-	}
-
-	// Handle this error in a sensible manner where possible
-	defer func() { _ = pusher.Stop(ctx) }()
-
-	fooKey := attribute.Key("ex.com/foo")
-	barKey := attribute.Key("ex.com/bar")
-	lemonsKey := attribute.Key("ex.com/lemons")
-	anotherKey := attribute.Key("ex.com/another")
-
-	commonAttributes := []attribute.KeyValue{
-		lemonsKey.Int(10),
-		attribute.String("A", "1"),
-		attribute.String("B", "2"),
-		attribute.String("C", "3"),
-	}
-
-	meter := global.Meter("ex.com/basic")
-
-	observerCallback := func(_ context.Context, result metric.Float64ObserverResult) {
-		result.Observe(1, commonAttributes...)
-	}
-	_ = metric.Must(meter).NewFloat64ValueObserver("ex.com.one", observerCallback,
-		metric.WithDescription("A ValueObserver set to 1.0"),
-	)
-
-	valueRecorder := metric.Must(meter).NewFloat64ValueRecorder("ex.com.two")
-
-	boundRecorder := valueRecorder.Bind(commonAttributes...)
-	defer boundRecorder.Unbind()
 
 	server := &http.Server{
 		Addr:         ":8443",
